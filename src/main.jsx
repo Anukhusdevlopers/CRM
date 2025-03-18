fe520304d3b700e00f863bf300080f3db62892c3
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2025-03-18 17:53:03 +0530

    removing extra context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ChatProvider } from './Context/Chat.context'
-
+ 
 createRoot(document.getElementById('root')).render(
-  <ChatProvider>
     <App />
-  </ChatProvider>
 )
